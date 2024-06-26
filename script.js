f3ff8bd077192d5aab01198437a53e77e2128e92
haumeale <haumealesya@gmail.com>
2024-06-26 17:52:18 +0300

    menu hidden

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const openBtn = document.querySelector('.header_search-btn')
-const modal = document.querySelector('.modal')
-const closeBtn = document.querySelector('.closeModal')
+const openBtn = document.getElementById('header_search-btn')
+const modal = document.getElementById('modal')
+const closeBtn = document.getElementById('closeModal')
 
 openBtn.addEventListener('click', () => {
     modal.style.display = 'block';
@@ -10,8 +10,14 @@ closeBtn.addEventListener('click', () => {
     modal.style.display = 'none';
 })
 
-// window.addEventListener('click', (event) => {
-//     if (event.target == modal){
-//         modal.style.display = 'none';
-//     }
-// })
+const hiddenOpen = document.getElementById('menu_burger')
+const hiddenMenu = document.getElementById('menu_hidden')
+const hiddenClose = document.getElementById('hidden_close')
+
+hiddenOpen.addEventListener('click', () => {
+    hiddenMenu.style.display = 'block';
+})
+
+hiddenClose.addEventListener('click', () => {
+    hiddenMenu.style.display = 'none';
+})
