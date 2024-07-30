82fd2c730b1b15c13407a8c93eb70d10057ebcf4
haumeale <haumealesya@gmail.com>
2024-07-30 16:52:39 +0300

    <3

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+<div class="info_resours">
+            <div class="card_resours">
+            <div class="detail_resours">
+                <p style="text-align: center;"><img src="/img/изображение 5.png" alt=""></p>
+
+            </div>
+        </div>
+        <div class="card_resours">
+            <div class="detail_resours">
+                <p style="text-align: center;"><img src="/img/изображение 5.png" alt=""></p>
+
+            </div>
+        </div>
+        <div class="card_resours">
+            <div class="detail_resours">
+                <p style="text-align: center;"><img src="/img/изображение 5.png" alt=""></p>
+
+            </div>
+        </div>
+        <div class="card_resours">
+            <div class="detail_resours">
+                <p style="text-align: center;"><img src="/img/изображение 5.png" alt=""></p>
+
+            </div>
+        </div>
+        
+
+        </div>
 
 
 
